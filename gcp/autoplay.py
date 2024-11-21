00b8eba0a61f051b692b6b97d5e884d088af0db0
백근호 <trueju0407@gmail.com>
2024-11-21 18:20:11 +0900

    add log-level

--- gcp/autoplay.py
+++ gcp/autoplay.py
@@ -34,6 +34,7 @@ class autoplay:
             options.add_argument('--lang=ja')
             options.add_argument('--disable-background-networking')
             options.add_argument('--disable-features=NetworkService,NetworkServiceInProcess')
+            options.add_argument('--log-level=3')
             options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134')
 
             try:
